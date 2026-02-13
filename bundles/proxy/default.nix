c849a8ad5b5fce7c9d07f02e9a137f4d37c0e0a1
Paul Skamrada <pl.skamrada@gmail.com>
2026-02-13 13:21:21 +0100

    Test if rebuilds work

---
+++ bundles/proxy/default.nix
@@ -0,0 +1,30 @@
+{ pkgs, ... }:
+{
+  imports = [
+    ../minimal-server
+  ];
+
+  networking = {
+    dhcpcd.enable = false;
+    useDHCP = false;
+    useHostResolvConf = false;
+  };
+
+  systemd.network = {
+    enable = true;
+    networks."50-eth0" = {
+      matchConfig.Name = "eth0";
+      networkConfig = {
+        DHCP = "ipv4";
+        IPv6AcceptRA = true;
+      };
+      linkConfig.RequiredForOnline = "routable";
+    };
+  };
+
+  environment.systemPackages = with pkgs; [
+    cowsay
+  ];
+
+  networking.firewall.enable = false;
+}
