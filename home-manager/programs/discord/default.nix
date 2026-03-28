96eff6b4ff12c271048baecf1c75f85d5732d8c2
Paul Skamrada <pl.skamrada@gmail.com>
2026-03-28 15:34:20 +0100

    Fix discord

--- home-manager/programs/discord/default.nix
+++ home-manager/programs/discord/default.nix
@@ -3,4 +3,21 @@
   home.packages = with pkgs; [
     discord
   ];
+
+  xdg.configFile."applications/discord.desktop".text = ''
+    [Desktop Entry]
+    Hidden=true
+  '';
+
+  xdg.desktopEntries.discord = {
+    name = "Discord";
+    genericName = "Chat";
+    exec = ''bash -lc "Discord"'';
+    icon = "discord";
+    terminal = false;
+    categories = [
+      "Network"
+      "InstantMessaging"
+    ];
+  };
 }
